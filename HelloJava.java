73e704d6d0e2f2c3867881b138035fb654265392
Deaglusha <51018642+Deaglusha@users.noreply.github.com>
2022-03-30 14:42:19 +0300

    Добавили BufferedReader

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Java!");
+
+        System.out.print("Введите приветствие: ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String text = bufferedReader.readLine();
+
+        System.out.println();
+        System.out.println(text);
     }
 }
